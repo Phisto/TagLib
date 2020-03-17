dd0cd07cd75e6c3d13ce3dc947cf75bdb798c8f0
Simon Gaus <phisto05@gmail.com>
2020-03-17 04:14:06 +0100

    Add umbrella header

---
+++ TagLibUmbrella.h
@@ -0,0 +1,14 @@
+//
+//  TagLibUmbrella.h
+//  TagLib
+//
+//  Created by Simon Gaus on 17.03.20.
+//  Copyright © 2020 sbooth.org. All rights reserved.
+//
+
+#ifndef TagLibUmbrella_h
+#define TagLibUmbrella_h
+
+
+#endif /* TagLibUmbrella_h */
+#import <TagLib/audioproperties.h>
